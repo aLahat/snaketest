2a745ebb740d4cd2814507734f58d050b4dcc8dd
Albert <lahat.albert@gmail.com>
2022-12-20 16:00:35 +0000

    first commit

---
+++ src/snaketest/snaketest.py
@@ -0,0 +1,34 @@
+from itertools import product
+class SM(object):
+    def __init__(self,**xargs):
+        self.items = xargs
+    def __getattribute__(self,attr):
+        if attr=="items":
+            return object.__getattribute__(self, "items")
+        attr =  object.__getattribute__(self, "items")[attr]
+        if type(attr)==str:
+            if ('{' in attr ) and ("wildcards" in globals()):
+                attr = attr.format(**wildcards.items)
+        if type(attr)==list:
+            new = []
+            for i in attr:
+                if type(i)==str:
+                    if ('{' in i ) and ("wildcards" in globals()):
+                        new.append(i.format(**wildcards.items))
+                    else:
+                        new.append(i)
+                else:
+                    new.append(i)
+            attr = new
+        return attr
+
+def expand(s,**xargs):
+    out = []
+    for a in (product(*[[(a,c) for c in b] for a,b in xargs.items()])):
+        out.append(s.format(**dict(a)))
+    return out
+
+temporary = lambda x:x
+protected = lambda x:x
+directory = lambda x:x
+ancient   = lambda x:x
